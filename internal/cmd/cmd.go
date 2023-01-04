5fafa2c887b03974783f7a934ce6e6b917778e85
hwipl <33433250+hwipl@users.noreply.github.com>
2023-01-04 13:03:10 +0100

    print MAC and details
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -6,7 +6,24 @@ import (
 	"github.com/hwipl/random-addr/internal/mac"
 )
 
+// printMAC prints m
+func printMAC(m *mac.MAC) {
+	fmt.Println("Random MAC Address")
+	fmt.Println("==================")
+	fmt.Println()
+	fmt.Println(m)
+	fmt.Println()
+	fmt.Println("Details")
+	fmt.Println("=======")
+	fmt.Println()
+	fmt.Println(m.Explain())
+	fmt.Println()
+	fmt.Println(m.Table())
+	fmt.Println()
+}
+
 // Run is the main entry point
 func Run() {
-	fmt.Println(mac.Random())
+	m := mac.Random()
+	printMAC(m)
 }
