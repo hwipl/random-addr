694eb5d8574dabb214452ab430f5bb14329b52b4
hwipl <33433250+hwipl@users.noreply.github.com>
2023-01-14 00:05:48 +0100

    add SetPrefix() to IPv6
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/ipv6/ipv6.go
+++ internal/ipv6/ipv6.go
@@ -21,6 +21,43 @@ func (ip *IPv6) String() string {
 	return ip.Addr().String()
 }
 
+// SetPrefix sets prefix in ip
+func (ip *IPv6) SetPrefix(prefix string) {
+	// parse prefix
+	p, err := netip.ParsePrefix(prefix)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get prefix bytes
+	b, err := p.MarshalBinary()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get number of bits to be overwritten
+	bits := p.Bits()
+
+	// overwrite bits
+	// try to overwrite full bytes first, then single bits
+	// ignore last byte of b because it's p.Bits()
+	for i := 0; i < len(b)-1; i++ {
+		if bits < 8 {
+			// last byte, not full, overwrite bits
+			ipBits := ip.b[i] & (0xff >> bits)
+			bBits := b[i] & (0xff << (8 - bits))
+			ip.b[i] = ipBits | bBits
+
+			break
+		}
+
+		// full byte, overwrite byte
+		ip.b[i] = b[i]
+
+		bits -= 8
+	}
+}
+
 // Random returns a random IPv6 address
 func Random() *IPv6 {
 	ip := &IPv6{}
