764354caace799004cdd4cda6baed8638dbba619
hwipl <33433250+hwipl@users.noreply.github.com>
2023-02-02 11:16:28 +0100

    add tests for IPv4
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/ipv4/ipv4_test.go
+++ internal/ipv4/ipv4_test.go
@@ -2,6 +2,28 @@ package ipv4
 
 import "testing"
 
+// TestSetPrefix tests SetPrefix of IPv4
+func TestSetPrefix(t *testing.T) {
+	ip := Random()
+	ip.SetPrefix("127.0.0.0/8")
+	want := "127.0.0.0"
+	got := ip.Network()
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+// TestSetPrefixLength tests SetPrefixLength of IPv4
+func TestSetPrefixLength(t *testing.T) {
+	ip := Parse("127.0.0.1")
+	ip.SetPrefixLength(8)
+	want := "127.0.0.0"
+	got := ip.Network()
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
 // TestRandom tests creation of random IPv4 addresses
 func TestRandom(t *testing.T) {
 	ip := Random()
